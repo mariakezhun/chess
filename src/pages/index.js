1825142325170a1b10780f2e72f294d8b0396ffc
Maria Kezhun <marusa.kezhoon@yandex.ru>
2024-06-08 06:45:23 +0300

    clean js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,33 +2,29 @@ let currentIndex = 0;
 
 function updateSlider() {
   const viewportWidth = window.innerWidth;
-  console.log(viewportWidth);
   const slidesPerView = 1;
-  const totalCards = document.querySelectorAll(".participants__card").length;
-  console.log(totalCards);
-  const counter = document.querySelector(".participants__counter");
+  const totalCards = document.querySelectorAll('.participants__card').length;
+  const counter = document.querySelector('.participants__counter');
 
   const totalViews = Math.ceil(totalCards / slidesPerView);
 
-  const cards = document.querySelector(".participants__cards");
+  const cards = document.querySelector('.participants__cards');
 
   if (viewportWidth < 1281) {
-    console.log("Ширина видимой области 1280 и меньше");
     cards.style.transform = `translateX(-${
       (currentIndex * 635.5) / totalViews
     }%)`;
     counter.textContent = `${currentIndex + 1}/6`;
-    document.querySelector(".participants__control_type_prev").disabled =
+    document.querySelector('.participants__control_type_prev').disabled =
       currentIndex === 0;
-    document.querySelector(".participants__control_type_next").disabled =
+    document.querySelector('.participants__control_type_next').disabled =
       currentIndex === totalCards - 1;
   } else {
     cards.style.transform = `translateX(-${currentIndex * 101.7}%)`;
-    console.log("Ширина видимой области больше 375");
     counter.textContent = `${currentIndex * 3 + 3}/6`;
-    document.querySelector(".participants__control_type_prev").disabled =
+    document.querySelector('.participants__control_type_prev').disabled =
       currentIndex === 0;
-    document.querySelector(".participants__control_type_next").disabled =
+    document.querySelector('.participants__control_type_next').disabled =
       currentIndex === totalCards / 3 - 1;
   }
 }
@@ -56,31 +52,27 @@ updateSlider();
 
 const slidesPerView = 2;
 const totalSlides = document.querySelectorAll(
-  ".transformation__grid-area"
+  '.transformation__grid-area'
 ).length;
-console.log(totalSlides);
 const totalViews = Math.ceil(totalSlides / slidesPerView + 1);
-console.log(totalViews);
-// let currentIndex = 0;
 
 function updateTransformSlider() {
-  const cards = document.querySelector(".transformation__container");
+  const cards = document.querySelector('.transformation__container');
   cards.style.transform = `translateX(-${(currentIndex * 530) / totalViews}%)`;
 
   updateIndicators();
 
-  document.querySelector(".transformation__control_type_prev").disabled =
+  document.querySelector('.transformation__control_type_prev').disabled =
     currentIndex === 0;
-  document.querySelector(".transformation__control_type_next").disabled =
+  document.querySelector('.transformation__control_type_next').disabled =
     currentIndex === totalViews - 1;
 }
-console.log(currentIndex);
 
 function updateIndicators() {
-  const indicators = document.querySelectorAll(".transformation__indicator");
+  const indicators = document.querySelectorAll('.transformation__indicator');
   indicators.forEach((indicator, index) => {
     indicator.classList.toggle(
-      "transformation__indicator_active",
+      'transformation__indicator_active',
       index === currentIndex
     );
   });
